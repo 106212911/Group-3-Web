be57476b3c4ecc02d5f364d0760bffe00b36ebbd
Pang Jun Hao <j24042195@student.newinti.edu.my>
2025-11-10 21:23:12 +0800

    Added SQL File

---
+++ project1/solidtech.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2025 at 02:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `solidtech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eoi`
+--
+
+CREATE TABLE `eoi` (
+  `EOInumber` int(11) NOT NULL,
+  `JobReference` varchar(5) NOT NULL,
+  `FirstName` varchar(20) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `DOB` date NOT NULL,
+  `Gender` enum('Male','Female','Others') NOT NULL,
+  `StreetAddress` varchar(40) NOT NULL,
+  `Suburb` varchar(40) NOT NULL,
+  `State` enum('VIC','NSW','QLD','NT','WA','SA','TAS','ACT') NOT NULL,
+  `Postcode` char(4) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Phone` varchar(15) NOT NULL,
+  `Skill1` tinyint(1) NOT NULL DEFAULT 0,
+  `Skill2` tinyint(1) NOT NULL DEFAULT 0,
+  `Skill3` tinyint(1) NOT NULL DEFAULT 0,
+  `OtherSkills` text NOT NULL,
+  `Status` enum('New','Current','Final') NOT NULL DEFAULT 'New'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `eoi`
+--
+ALTER TABLE `eoi`
+  ADD PRIMARY KEY (`EOInumber`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `eoi`
+--
+ALTER TABLE `eoi`
+  MODIFY `EOInumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
